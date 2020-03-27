8afa17c8ca68e5e7e4f2dd350ea5a9290de548cc
Ivo Fernandes <ivoadf@gmail.com>
2020-03-27 19:41:12 +0000

    Initial commit with config, character data and minimap.

---
+++ update_repo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+TIBIA_CONFIG_DIR="$HOME/.local/share/CipSoft GmbH/Tibia/packages/Tibia/"
+cp -r -u "$TIBIA_CONFIG_DIR/characterdata" "$TIBIA_CONFIG_DIR/conf" "$TIBIA_CONFIG_DIR/minimap" .
